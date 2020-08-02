abfeea69f909a73cd4b81603212c1817c4f1a379
Dent24 <cljhs90607@gmail.com>
2020-08-02 11:29:26 +0800

    新增JSON資料以及連接

--- ajax.js
+++ ajax.js
@@ -7,3 +7,16 @@ xhr.send();
 xhr.onload = () => {
     contant.innerText = xhr.responseText;
 }
+
+var product = document.getElementById("product");
+var price = document.getElementById("price")
+
+var xhrJson = new XMLHttpRequest();
+xhrJson.open("GET","./data.json",true);
+xhrJson.send();
+
+xhrJson.onload = () => {
+    var x = JSON.parse(xhrJson.responseText);
+    product.innerText = "產品名稱:" + x["product"];
+    price.innerText = "產品價格:" + x["price"];
+}
